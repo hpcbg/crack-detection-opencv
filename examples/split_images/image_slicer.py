29357178acc1727fd5030500d84ab0841883a42c
Simeon Tsvetanov <simeon.tsvetanov@gmail.com>
2022-06-25 16:25:29 +0300

    Added image_split.py

---
+++ examples/split_images/image_slicer.py
@@ -0,0 +1,8 @@
+# https://pypi.org/project/image-slicer/
+# https://stackoverflow.com/questions/63798231/slice-images-in-directory-using-glob-image-slicer-based-on-pixel-sizes-instead
+
+import glob, os
+import image_slicer
+
+for file in glob.glob('~\\test_folder\\*.jpg'):
+    image_slicer.slice(file, row=3, col=4)
--- examples/split_images/image_split.py
+++ examples/split_images/image_split.py
@@ -3,9 +3,9 @@ import cv2
 import numpy as np
 from os import listdir
 from os.path import isfile, join
-from pathlib import Path
 import argparse
-import numpy
+
+split_size = 100
 
 # Argument parsing variable declared
 ap = argparse.ArgumentParser()
@@ -24,11 +24,11 @@ args = vars(ap.parse_args())
 input_path = args["input"]
 output_path = args["output"]
 file_names = [f for f in listdir(input_path) if isfile(join(input_path, f))]
-images = numpy.empty(len(file_names), dtype=object)
+images = np.empty(len(file_names), dtype=object)
 
 # Iterate through every image
 # and resize all the images.
-for n, file_name in file_names:
+for n, file_name in enumerate(file_names):
     path = join(input_path, file_name)
     images[n] = cv2.imread(join(input_path, file_name),
                            cv2.IMREAD_UNCHANGED)
@@ -36,19 +36,10 @@ for n, file_name in file_names:
     # Load the image in img variable
     img = cv2.imread(path, 1)
 
-    # Define a resizing Scale
-    # To declare how much to resize
-    resize_scaling = 50
-    resize_width = int(img.shape[1] * resize_scaling / 100)
-    resize_height = int(img.shape[0] * resize_scaling / 100)
-    resized_dimensions = (resize_width, resize_height)
-
-    # Create resized image using the calculated dimensions
-    resized_image = cv2.resize(img, resized_dimensions,
-                               interpolation=cv2.INTER_AREA)
-
+    print(f'Processing image: {n}')
     # Save the image in Output Folder
-    cv2.imwrite(
-        f'{output_path}/{resize_width}x{resize_height}_{n}_resized.jpg', resized_image)
+    for r in range(0, img.shape[0], split_size):
+        for c in range(0, img.shape[1], split_size):
+            cv2.imwrite(f"{output_path}/img_{n}_{r}_{c}.png", img[r:r + split_size, c:c + split_size, :])
 
-print("Images resized Successfully")
+
